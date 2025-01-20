3f00cd98e14491655b3ebebfb6f4111ceaa3c122
Muhibbulloh Hayitmurodov <181156023+santebaratie@users.noreply.github.com>
2025-01-20 12:07:08 +0500

    Set lower/upper limits when page loaded

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 "use strict";
+
+// Select UI elements
+const plusBtn = document.querySelector(".fa-plus");
+const minusBtn = document.querySelector(".fa-minus");
+const resetBtn = document.querySelector(".btn--reset");
+const autoCountBtn = document.querySelector(".btn--auto");
+const lowerLimitInput = document.querySelector("#lower-limit");
+const upperLimitInput = document.querySelector("#upper-limit");
+
+
+// Initiate global variables;
+
+let counter = 0;
+
+// Set the upper and lower limits when page loaded
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  lowerLimitInput.value = 0;
+  upperLimitInput.value = 100;
+})
+
+
